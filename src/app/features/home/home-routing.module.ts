7df20a31aab1953b52244910035eb69db3069083
Marcos Andrade <marcospandrade2@gmail.com>
2022-07-02 16:11:13 -0300

    chore: home page module add ngx-bootstrap

--- src/app/features/home/home-routing.module.ts
+++ src/app/features/home/home-routing.module.ts
@@ -1,16 +1,16 @@
+import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: async () =>
-      await import('./features/home/home.module').then((m) => m.HomeModule),
+    component: HomeComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class HomeRoutingModule {}
---
+++ src/app/features/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { NavbarModule } from 'src/app/shared/navbar/navbar.module';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, NavbarModule],
+})
+export class HomeModule {}
