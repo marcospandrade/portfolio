24a7fa19bbfd7dd9cae7cf4bf81c5a8440a4ab85
Marcos Andrade <marcospandrade2@gmail.com>
2022-07-04 15:11:49 -0300

    add animation on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import * as aos from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-portfolio';
+
+  ngOnInit() {
+    aos.init();
+  }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,22 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fade', [
+      state('*', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(800)]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
