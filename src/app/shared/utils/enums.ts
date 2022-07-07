e74eb7f04c7b7b7d4dfaec5ac488fbc5349c8b97
Marcos Andrade <marcospandrade2@gmail.com>
2022-07-07 14:49:07 -0300

    change project image, write text about projects

--- src/app/shared/utils/enums.ts
+++ src/app/shared/utils/enums.ts
@@ -3,4 +3,5 @@ export enum linksSocialMedia {
   "linkedin" = "https://www.linkedin.com/in/marcsandrade/",
   "instagram" = "https://www.instagram.com/marcs.andrade/",
   "github" = "https://github.com/marcospandrade",
+  "curriculo" = "https://drive.google.com/file/d/1hI1qkXERzd2MmqmiY95bl3zjVtMvfytj/view?usp=sharing",
 }
